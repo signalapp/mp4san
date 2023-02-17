924a1be0f51c2f2a955f7de5632f981012861169
Lilith Silvestris <120892488+oftheforest@users.noreply.github.com>
2023-02-17 23:57:34 +0000

    Add Mp4Box::type_()
    Add a type_() method to the trait and derive macro. Fix size() to include the
    size of the type field.
    
    Extra changes:
    * Write docs for the Mp4Box trait
    * Mark the generated trait impls as #[automatically_derived]
    * Emit a compiler error instead of panicking in the derive macro
    * Make the test names better

--- parser/proc_macros/src/lib.rs
+++ parser/proc_macros/src/lib.rs
@@ -2,26 +2,98 @@ use proc_macro::TokenStream;
 
 use proc_macro2::TokenStream as TokenStream2;
 use quote::{quote, quote_spanned};
-use syn::{parse_macro_input, Data, DeriveInput};
+use syn::{parse_macro_input, Data, DeriveInput, Lit, Meta};
 use syn::spanned::Spanned;
+use uuid::Uuid;
 
-#[proc_macro_derive(Mp4Box)]
+#[proc_macro_derive(Mp4Box, attributes(box_type))]
 pub fn derive_mp4_box(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let ident = &input.ident;
     let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
 
+    let box_type = extract_box_type(&input);
     let size = sum_box_size(&input);
 
     let expanded = quote! {
+        #[automatically_derived]
         impl #impl_generics mp4san_isomparse::Mp4Box for #ident #ty_generics #where_clause {
             fn size(&self) -> std::primitive::u64 {
                 std::convert::TryFrom::try_from(#size).unwrap()
+            }
+
+            fn type_(&self) -> mp4san_isomparse::BoxType {
+                #box_type
             }
         }
     };
 
     TokenStream::from(expanded)
+}
+
+fn extract_box_type(input: &DeriveInput) -> TokenStream2 {
+    let mut iter = input.attrs.iter().filter(|attr| attr.path.is_ident("box_type"));
+    let Some(attr) = iter.next() else {
+        // When emitting compiler errors, no semicolon should be placed after `compile_error!()`:
+        // doing so will generate extraneous errors (type mismatch errors, Rust parse errors, or the
+        // like) in addition to the error we intend to emit.
+        return quote! { std::compile_error!("missing `#[box_type]` attribute") }.into();
+    };
+    if let Some(extra_attr) = iter.next() {
+        return quote_spanned! { extra_attr.span() =>
+            std::compile_error!("more than one `#[box_type]` attribute is not allowed")
+        }.into();
+    }
+    let lit = match attr.parse_meta() {
+        Ok(Meta::NameValue(name_value)) => name_value.lit,
+        _ => return quote_spanned! { attr.span() =>
+            std::compile_error!("`box_type` attribute must be of the form `#[box_type = ...]`")
+        }.into(),
+    };
+    match &lit {
+        Lit::Int(int_lit) => {
+            let int = match int_lit.base10_parse::<u128>() {
+                Ok(int) => int,
+                Err(error) => return error.into_compile_error(),
+            };
+            if let Ok(int) = u32::try_from(int) {
+                return quote! { mp4san_isomparse::BoxType::Compact(#int) };
+            } else {
+                return quote! { mp4san_isomparse::BoxType::Extended(uuid::Uuid::from_u128(#int)) };
+            }
+        },
+        Lit::Str(string_lit) => {
+            let string = string_lit.value();
+            if let Ok(uuid) = Uuid::parse_str(&string) {
+                let int = uuid.as_u128();
+                return quote! { mp4san_isomparse::BoxType::Extended(uuid::Uuid::from_u128(#int)) };
+            } else if string.len() == 4 {
+                return quote! {
+                    let type_string = #string_lit;
+                    let type_ = std::primitive::u32::from_be_bytes(
+                        std::convert::TryInto::try_into(type_string.as_bytes()).unwrap(),
+                    );
+                    mp4san_isomparse::BoxType::Compact(type_)
+                };
+            }
+        },
+        Lit::ByteStr(bytes_lit) => {
+            let bytes = bytes_lit.value();
+            if bytes.len() == 4 {
+                return quote! {
+                    let type_bytes = *#bytes_lit;
+                    let type_ = std::primitive::u32::from_be_bytes(type_bytes);
+                    mp4san_isomparse::BoxType::Compact(type_)
+                };
+            }
+        },
+        _ => {},
+    }
+    quote_spanned! { lit.span() => std::compile_error!(concat!(
+        r#"malformed `box_type` attribute input: try `"moov"`, `b"moov"`, or `0x6d6f6f76` for a"#,
+        r#" compact type, or `"a7b5465c-7eac-4caa-b744-bdc340127d37"` or"#,
+        r#" `0xa7b5465c_7eac_4caa_b744_bdc340127d37` for an extended type"#,
+    )) }
 }
 
 fn sum_box_size(derive_input: &DeriveInput) -> TokenStream2 {
@@ -47,7 +119,18 @@ fn sum_box_size(derive_input: &DeriveInput) -> TokenStream2 {
             });
             quote! { + match self { #(#arms)* } }
         },
-        Data::Union(_) => panic!("this trait cannot be derived for unions"),
+        Data::Union(_) => return quote! {
+            std::compile_error!("this trait cannot be derived for unions")
+        },
     };
-    quote! { std::mem::size_of::<std::primitive::u32>() #sum_expr }
+    quote! {
+        // size
+        std::mem::size_of::<std::primitive::u32>()
+        // type
+        + std::mem::size_of::<std::primitive::u32>()
+        // usertype
+        + if self.type_().is_extended() { std::mem::size_of::<[std::primitive::u8; 16]>() } else { 0 }
+        // whatever fields the box has
+        #sum_expr
+    }
 }
--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -1,30 +1,88 @@
+// Used by the derive macros' generated code.
 use crate as mp4san_isomparse;
 
 pub use mp4san_isomparse_macros::Mp4Box;
+pub use uuid::Uuid;
 
+/// An object (box, atom) in the mp4 file structure.
+///
+/// A box is defined by its [type identifier](BoxType) and its [size](Mp4Box::size).
 pub trait Mp4Box {
+    /// Returns the size (length) of the box.
     fn size(&self) -> u64;
+
+    /// Returns the type identifier of the box.
+    ///
+    /// Since each box type is modeled as a separate type in Rust, this could have been an
+    /// associated function (with no `self`) or even an associated const. It is modeled as a method,
+    /// however, because either of those alternatives would make the trait non-object-safe.
+    fn type_(&self) -> BoxType;
+}
+
+/// The type code of an mp4 box.
+///
+/// Every box has a type. Boxes defined by ISO standard have a _compact_, u32 type; other boxes have
+/// an _extended_, UUID type.
+///
+/// Extended types of the form `XXXXXXXX-0011-0010-8000-00aa00389b71` are reserved by ISO to
+/// represent compact types (the first 32 bits, shown as `XXXXXXXX`, hold the compact type code
+/// being represented). **These extended types should not be used:** files containing them are not
+/// compliant with the specification, implementations are explicitly not required to recognize
+/// them, and this implementation in particular will treat them as unknown extended types.
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub enum BoxType {
+    Compact(u32),
+    Extended(Uuid),
+}
+
+impl BoxType {
+    /// Returns `true` if the type is a compact/standard type.
+    pub fn is_compact(self) -> bool {
+        !self.is_extended()
+    }
+
+    /// Returns `true` if the type is an extended/private type.
+    ///
+    /// This is the opposite of [`is_compact`].
+    pub fn is_extended(self) -> bool {
+        matches!(self, BoxType::Extended(_))
+    }
 }
 
 #[derive(Mp4Box)]
+#[box_type = b"\xffX0\x00"]
 pub struct NotARealBox {
     pub bar_ax: u64,
     pub foo_by: u32,
 }
 
 #[derive(Mp4Box)]
+#[box_type = 0xff583001]
 pub enum FakeEnumBox {
     A { foo: u32 },
     B(u64),
     C,
 }
 
+#[derive(Mp4Box)]
+#[box_type = 4283969538] // 0xff583002
+pub struct AnotherFakeBox;
+
+#[derive(Mp4Box)]
+#[box_type = "c12fdd3f-1e93-464c-baee-7c4480628f58"]
+pub struct FakeUuidTypeBox;
+
+#[derive(Mp4Box)]
+#[box_type = "xa04"]
+pub struct Fifth;
+
 //impl Mp4Box for NotARealBox {
 //    fn size(&self) -> u64 {
 //        0 + size_of::<u64>() + size_of::<u32>()
 //    }
-//    //XXX: fn type_(&self) -> u32 | u128 { .. }
-//    //XXX: and then the size() computation calls type_() to see whether it's 4 bytes or 20(?) bytes
+//    fn type_(&self) -> BoxType {
+//        BoxType::Compact(/* whatever the #[box_type] says */)
+//    }
 //}
 
 #[cfg(test)]
@@ -32,29 +90,69 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_1() {
+    fn test_size_simple() {
         let not_a_real = NotARealBox {
             bar_ax: u64::MAX,
             foo_by: u32::MAX,
         };
-        assert_eq!(not_a_real.size(), 4 + 8 + 4);
+        assert_eq!(not_a_real.size(), 4 + 4 + 8 + 4);
     }
 
     #[test]
-    fn test_2() {
+    fn test_size_enum_a() {
         let fake_enum = FakeEnumBox::A { foo: u32::MAX };
+        assert_eq!(fake_enum.size(), 4 + 4 + 4);
+    }
+
+    #[test]
+    fn test_size_enum_b() {
+        let fake_enum = FakeEnumBox::B(u64::MAX);
+        assert_eq!(fake_enum.size(), 4 + 4 + 8);
+    }
+
+    #[test]
+    fn test_size_enum_c() {
+        let fake_enum = FakeEnumBox::C;
         assert_eq!(fake_enum.size(), 4 + 4);
     }
 
     #[test]
-    fn test_3() {
-        let fake_enum = FakeEnumBox::B(u64::MAX);
-        assert_eq!(fake_enum.size(), 4 + 8);
+    fn test_size_exttype() {
+        let fake_box = FakeUuidTypeBox;
+        assert_eq!(fake_box.size(), 4 + 4 + 16);
     }
 
     #[test]
-    fn test_4() {
-        let fake_enum = FakeEnumBox::C;
-        assert_eq!(fake_enum.size(), 4);
+    fn test_type_bytes() {
+        let not_a_real = NotARealBox {
+            bar_ax: u64::MAX,
+            foo_by: u32::MAX,
+        };
+        assert_eq!(not_a_real.type_(), BoxType::Compact(0xff583000));
+    }
+
+    #[test]
+    fn test_type_compact_int_hex() {
+        let fake_enum = FakeEnumBox::A { foo: u32::MAX };
+        assert_eq!(fake_enum.type_(), BoxType::Compact(0xff583001));
+    }
+
+    #[test]
+    fn test_type_compact_int_decimal() {
+        let fake_box = AnotherFakeBox;
+        assert_eq!(fake_box.type_(), BoxType::Compact(0xff583002));
+    }
+
+    #[test]
+    fn test_type_extended() {
+        let fake_box = FakeUuidTypeBox;
+        let expected = BoxType::Extended(Uuid::from_u128(0xc12fdd3f_1e93_464c_baee_7c4480628f58));
+        assert_eq!(fake_box.type_(), expected);
+    }
+
+    #[test]
+    fn test_type_compact_str() {
+        let fake_box = Fifth;
+        assert_eq!(fake_box.type_(), BoxType::Compact(0x78613034));
     }
 }
