dd4d642a4cf0976ed618c221edc082af87d2fd8e
Jessa <git@jessa0.com>
2023-03-10 11:06:46 -0800

    remove implicit conversion to bool in ensure_attach!()

--- mp4san/src/macros.rs
+++ mp4san/src/macros.rs
@@ -5,11 +5,12 @@ macro_rules! bail_attach {
 }
 
 macro_rules! ensure_attach {
-    ($cond:expr, $err:expr, $($attachment:expr),+ $(,)?) => {
-        if !bool::from($cond) {
+    ($cond:expr, $err:expr, $($attachment:expr),+ $(,)?) => {{
+        let cond: bool = $cond;
+        if !cond {
             bail_attach!($err, concat!("condition failed: ", stringify!($cond)), $($attachment),+);
         }
-    };
+    }};
 }
 
 macro_rules! report_attach {
