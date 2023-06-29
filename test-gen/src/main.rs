c6490042ae0ed5394234fa7ce9ac4b7937992e1d
Jessa <git@jessa0.com>
2023-06-29 10:34:48 -0700

    fix invalid test input file

--- test-gen/src/main.rs
+++ test-gen/src/main.rs
@@ -1,11 +1,11 @@
 use std::fs::File;
-use std::io::{self, BufRead, BufReader, BufWriter, Read};
+use std::io::{self, BufRead, BufReader, BufWriter, Read, Write};
 use std::path::PathBuf;
 
 use anyhow::Context;
 use clap::Parser as _;
 use libflate::gzip;
-use mp4::{BoxHeader, BoxType, HEADER_SIZE};
+use mp4san::parse::{BoxHeader, BoxType};
 
 /// A tool to minify MP4 files (by removing their video data) for use as test input to mp4san.
 #[derive(clap::Parser)]
@@ -29,16 +29,29 @@ fn main() -> Result<(), anyhow::Error> {
     let mut reader = BufReader::new(input);
     let mut encoder = gzip::Encoder::new(BufWriter::new(output)).context("Error writing to output")?;
 
-    while !reader.fill_buf()?.is_empty() {
-        let header = BoxHeader::read(&mut reader)?;
-        header.write(&mut encoder).context("Error writing to output")?;
-
-        let mut data_reader: Box<dyn Read> = match header.size {
-            0 => Box::new(&mut reader),
-            _ => Box::new(reader.by_ref().take(header.size - HEADER_SIZE)),
+    while !reader.fill_buf().context("Error reading input file")?.is_empty() {
+        let mut header_data = vec![0; 8];
+        reader
+            .read_exact(&mut header_data)
+            .context("Error reading input file")?;
+        let header = match BoxHeader::parse(&header_data[..]) {
+            Ok(header) => header,
+            Err(_) => {
+                header_data.extend([0; 8]);
+                reader
+                    .read_exact(&mut header_data[8..])
+                    .context("Error reading input file")?;
+                BoxHeader::parse(&header_data[..]).context("Error reading input file")?
+            }
         };
-        match header.name {
-            BoxType::MdatBox => {
+        encoder.write_all(&header_data).context("Error writing to output")?;
+
+        let mut data_reader: Box<dyn Read> = match header.box_data_size().context("Error reading input file")? {
+            None => Box::new(&mut reader),
+            Some(box_data_size) => Box::new(reader.by_ref().take(box_data_size)),
+        };
+        match header.box_type() {
+            BoxType::MDAT => {
                 let data_len = io::copy(&mut data_reader, &mut io::sink()).context("Error reading input")?;
                 io::copy(&mut io::repeat(0).take(data_len), &mut encoder).context("Error writing to output")?;
             }
