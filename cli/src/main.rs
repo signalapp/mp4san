57cda0196973ba08ea23a508d88fa27ed31fd826
Lilith Silvestris <120892488+oftheforest@users.noreply.github.com>
2023-10-26 15:11:53 -0700

    cli: Add support for webpsan

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -2,11 +2,24 @@ use std::fs::File;
 use std::path::PathBuf;
 
 use anyhow::Context;
-use clap::Parser as _;
+use clap::{Parser as _, ValueEnum};
 
 #[derive(clap::Parser)]
 struct Args {
+    /// The format of the media file.
+    ///
+    /// If not specified, a guess will be made based on the file extension.
+    #[clap(long, short = 't')]
+    format: Option<Format>,
+
+    /// Path to the file to test sanitization on.
     file: PathBuf,
+}
+
+#[derive(Debug, Clone, Copy, ValueEnum)]
+enum Format {
+    Mp4,
+    Webp,
 }
 
 fn main() -> Result<(), anyhow::Error> {
@@ -18,9 +31,21 @@ fn main() -> Result<(), anyhow::Error> {
 
     let args = Args::try_parse().context("Error parsing command line arguments")?;
 
-    let file = File::open(args.file).context("Error opening mp4 file")?;
+    let format = match args.format {
+        Some(t) => t,
+        None => {
+            let extension = args.file.extension().unwrap_or_default();
+            ValueEnum::from_str(&extension.to_string_lossy(), true)
+                .map_err(|_| anyhow::anyhow!("can't guess media format (unrecognized extension {extension:?})"))?
+        }
+    };
 
-    mp4san::sanitize(file).context("Error parsing mp4 file")?;
+    let file = File::open(args.file).context("Error opening file")?;
+
+    match format {
+        Format::Mp4 => mp4san::sanitize(file).map(drop).context("Error parsing mp4 file")?,
+        Format::Webp => webpsan::sanitize(file).context("Error parsing webp file")?,
+    }
 
     Ok(())
 }
