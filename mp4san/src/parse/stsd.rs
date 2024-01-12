6dfb570e0e1f2b81eaab8a48faafba8c8fe3002a
Jordan Rose <jrose@signal.org>
2024-01-12 13:44:58 -0800

    stsd box includes full header

--- mp4san/src/parse/stsd.rs
+++ mp4san/src/parse/stsd.rs
@@ -2,10 +2,13 @@
 
 use derive_more::{Deref, DerefMut};
 
-use super::{BoundedBoxes, ParseBox, ParsedBox};
+use super::{BoundedBoxes, ConstFullBoxHeader, ParseBox, ParsedBox};
 
 #[derive(Clone, Debug, Default, Deref, DerefMut, ParseBox, ParsedBox)]
 #[box_type = "stsd"]
 pub struct StsdBox {
+    pub _parsed_header: ConstFullBoxHeader,
+    #[deref]
+    #[deref_mut]
     pub children: BoundedBoxes<u32>,
 }
